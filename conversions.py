4b46d0f3bceb9f48cfe535f307106dbe2d0a82c0
GetFlawed <cairokv62@gmail.com>
2021-06-01 15:43:50 +0100

    Added menu, testing out different functions

--- conversions.py
+++ conversions.py
@@ -1,8 +1,35 @@
-# This is a test for a github commit number 3
-# Hello World
-# This is a test. Please add a new line if you can see this. Venv folder also added
-print("Hello World")
+def menu():
+    print("""
+    Menu
+    
+    Weight - (Kilograms (Kg), Grams (g), Pounds (lbs))
+    Volume - (Litres (l), Millilitres (ml), Fluid Ounces (fl oz))
+    Temperature - (Celsius (C), Fahrenheit (F))
+    """)
+    input_type = input("\nPlease select which measurement you wish to enter: ")
+    weight_array = ("kilograms", "kg", "grams", "g", "pounds", "lbs", "weight")
+    if input_type.lower() in weight_array:
+        weight(input_type)
+    elif input_type.lower() == "litres" or "l" or "millilitres" or "ml" or "fluid ounces" or "fl oz" or "volume":
+        volume(input_type)
+    elif input_type.lower() == "celsius" or "c" or "fahrenheit" or "f" or "temperature":
+        temperature(input_type)
+    else:
+        print("Incorrect spelling or measurement type.")
+        print("Please make sure to enter a measurement that is on the provided list")
+        menu()
 
-x = 5
-y = 10
-print(x + y)
+
+def weight(input_type):
+    input_value = input("\nPlease enter how many Kg you wish to convert: ")
+
+
+def volume(input_type):
+    print("test2")
+
+
+def temperature(input_type):
+    print("test3")
+
+
+menu()
